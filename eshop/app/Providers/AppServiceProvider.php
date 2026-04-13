a99eabcdf5c98cf9a474e41cbb5f288f54c080b7
kavonada <kavonada@gmail.com>
2026-04-13 17:55:37 +0200

    Pocet poloziek v kosiku

--- eshop/app/Providers/AppServiceProvider.php
+++ eshop/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Cart;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+
+            if (Auth::check()) {
+                $cart = Cart::where('user_id', Auth::id())->first();
+
+                $count = $cart 
+                    ? $cart->cartItems()->sum('quantity') 
+                    : 0;
+
+            } else {
+                $count = collect(session('cart', []))->sum('quantity');
+            }
+
+            $view->with('cartCount', $count);
+        });
     }
 }
